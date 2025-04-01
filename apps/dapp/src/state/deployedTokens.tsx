1f4a704b44e43265fbf3c1c496211752abadea87
Burtonium <9215927+Burtonium@users.noreply.github.com>
2025-04-01 16:22:14 +0700

    Remove onchange

--- apps/dapp/src/state/deployedTokens.tsx
+++ apps/dapp/src/state/deployedTokens.tsx
@@ -1,4 +1,4 @@
-import { Token, TokenList } from "@axis-finance/types";
+import { Token } from "@axis-finance/types";
 import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { useCallback } from "react";
@@ -10,37 +10,18 @@ export const deployedTokensAtom = atomWithStorage<Token[] | null>(
   { getOnInit: true },
 );
 
-export const useDeployedTokens = ({
-  onChange,
-}: { onChange?: (token: TokenList) => void } = {}) => {
+export const useDeployedTokens = () => {
   const [data, set] = useAtom(deployedTokensAtom);
 
   const add = useCallback(
     (token: Token) => {
       set((prev) => {
         const tokens = [...(prev ?? []), token];
-        onChange?.({
-          name: "Custom",
-          logoURI: "https://www.fillmurray.com/100/100",
-          tokens: tokens.map((t) => ({
-            address: t.address,
-            chainId: t.chainId,
-            decimals: t.decimals,
-            name: t.name,
-            symbol: t.symbol,
-            totalSupply: t.totalSupply,
-          })),
-          timestamp: "",
-          version: {
-            major: 0,
-            minor: 0,
-            patch: 0,
-          },
-        });
+
         return tokens;
       });
     },
-    [onChange, set],
+    [set],
   );
 
   const remove = useCallback(
